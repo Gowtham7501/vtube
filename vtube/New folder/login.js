918e21257d13c0a7d8371a7a64d782378b1116de
Gowtham7501 <115854250+Gowtham7501@users.noreply.github.com>
2024-01-11 18:02:03 +0530

    Add files via upload

---
+++ vtube/New folder/login.js
@@ -0,0 +1,39 @@
+ /* CSS for form container */
+ .form-container {
+    margin: 0 auto;
+    width: 300px;
+    padding: 20px;
+    background-color: #f2f2f2;
+  }
+  
+  /* CSS for input fields */
+  .form-container input[type="text"],
+  .form-container input[type="password"] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  
+  /* CSS for hide/show password checkbox */
+  .form-container input[type="checkbox"] {
+    margin-top: 12px;
+  }
+  
+  /* CSS for submit button */
+  .form-container input[type="submit"] {
+    background-color: #4caf50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
+  
+  /* CSS for text inside submit button on hover */
+  .form-container input[type="submit"]:hover {
+    background-color: #45a049;
+  }
